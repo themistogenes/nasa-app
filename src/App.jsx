137baba149c56ce693bbf701b319661f55b61319
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-05 10:15:01 -0400

    create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Main from "./components/Main"
+import SideBar from "./components/SideBar"
+import Footer from "./components/Footer"
 
 function App() {
-
   return (
     <>
-
+      <SideBar></SideBar>
+      <Main />
+      <Footer />
     </>
   )
 }
