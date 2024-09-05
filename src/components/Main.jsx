1be8be6f83a00f6ddc4add38a2c91ccc5d43bc31
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-05 10:36:58 -0400

    add sample content

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,7 @@
 export default function Main() {
   return (
     <div>
-      Main
+      <img src="mars.png" alt="mars-demo-picture" />
     </div>
   )
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,13 @@
 export default function SideBar() {
   return (
     <div>
-      SideBar
+      <h2>The Brutal Martian Landscape</h2>
+      <div>
+        <p>Description</p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi iste, consectetur excepturi perferendis temporibus quo ducimus ea quaerat vitae blanditiis impedit optio ab ex assumenda, possimus modi voluptatum provident explicabo!
+        </p>
+      </div>
     </div>
   )
 }
