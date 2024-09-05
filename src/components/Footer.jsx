91cde5ea1a64dfa66d7728efd6669c97a64b61f3
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-05 11:31:07 -0400

    create div in footer for background gradient

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 export default function Footer() {
   return (
     <footer>
+      <div className="bgGradient"></div>
       <div>
         <h2>The Brutal Martian Landscape</h2>
         <h1>APOD PROJECT</h1>
