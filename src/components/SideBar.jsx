9de4f09a1a9fc87fc28e94d2373148aa672f5ac2
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-05 11:31:52 -0400

    create divs in sidebar for background overlay and content

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,18 @@
 export default function SideBar() {
   return (
-    <div>
-      <h2>The Brutal Martian Landscape</h2>
-      <div>
-        <p>Description</p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi iste, consectetur excepturi perferendis temporibus quo ducimus ea quaerat vitae blanditiis impedit optio ab ex assumenda, possimus modi voluptatum provident explicabo!
-        </p>
+    <div className="sidebar">
+      <div className="bgOverlay"></div>
+      <div className="sidebarContent">
+        <h2>The Brutal Martian Landscape</h2>
+        <div>
+          <p>Description</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi iste, consectetur excepturi perferendis temporibus quo ducimus ea quaerat vitae blanditiis impedit optio ab ex assumenda, possimus modi voluptatum provident explicabo!
+          </p>
+        </div>
+        <button>
+          <i className="fa-solid fa-arrow-right"></i>
+        </button>
       </div>
     </div>
   )
