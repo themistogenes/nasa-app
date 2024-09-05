93e5d86d5c3e11c7783da000be29197fdb1eb902
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-05 12:25:18 -0400

    fetch data from API and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Main from "./components/Main"
 import SideBar from "./components/SideBar"
 import Footer from "./components/Footer"
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   function handleToggleModal() {
     setShowModal(!showModal);
   }
 
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        setData(apiData);
+        console.log('DATA\n', apiData);
+      } catch (err) {
+        console.log(err.message);
+      }
+    }
+
+    fetchAPIData();
+  }, [])
+
   return (
     <>
-      <Main />
+      {
+        data ? (
+          <Main />
+        ) : (
+          <div className="loadingState">
+            <i className="fa-solid fa-gear"></i>
+          </div>
+        )}
       {
         showModal && (
           <SideBar 
@@ -21,7 +47,13 @@ function App() {
           </SideBar>
         )
       }
-      <Footer handleToggleModal={handleToggleModal} />
+      {
+        data && (
+          <Footer 
+            handleToggleModal={handleToggleModal} 
+          />
+        )
+      }
     </>
   )
 }
